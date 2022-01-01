bbb41969e83a35041407fdfdae4fab49c92001c9
zhengc <zhengc12@uw.edu>
2021-12-31 20:34:05 -0800

    finished the implementation of the page design for each blog! Happy New Year~

--- src/main/java/com/blog_zheng/myblog/service/CommentServiceImpl.java
+++ src/main/java/com/blog_zheng/myblog/service/CommentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -39,6 +40,46 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public List<Comment> getAllComments(Long blogID) {
         Sort sort = Sort.by(Sort.Direction.ASC, "date");
-        return commentRepository.findByBlogBlogID(blogID, sort);
+        List<Comment> comments = commentRepository.findByBlogBlogID(blogID, sort);
+        return recurseReply(comments);
+    }
+
+    // Get the replies of each comment
+    // All recursive replies belong to the current topmost comment, therefore,
+    // instead of having multiple layers for each reply, we have only two layers:
+    // comment section structure is like this:
+    // --- topmost comment(0)
+    // --- --- reply1-0
+    // --- --- reply2-1
+    // --- --- reply1-2
+    // --- --- reply3-0
+    //
+    // Return a list of comments where each comment contains all its replies
+    private List<Comment> recurseReply(List<Comment> comments) {
+        List<Comment> result = new ArrayList<>();
+        for (Comment comment : comments) {
+            // we first find the topmost comment
+            if (comment.getParentComment() == null) {
+                List<Comment> replies = comment.getReplies();
+                List<Comment> combinedReplies = new ArrayList<>();
+
+                // store all recursive sub-replies to the combinedReplies list
+                for (Comment reply : replies) {
+                    helper(reply, combinedReplies);
+                }
+                comment.setReplies(combinedReplies);
+                result.add(comment);
+            }
+        }
+        return result;
+    }
+
+    private void helper(Comment reply, List<Comment> list) {
+        if (!list.contains(reply)) {
+            list.add(reply);
+        }
+        for (Comment subReply : reply.getReplies()) {
+            helper(subReply, list);
+        }
     }
 }
